c7c10718546b020173c504d5b109e4246a5f627a
AndreanLNA <83387525+AndreanLNA@users.noreply.github.com>
2022-04-21 17:49:45 +0700

    Add files via upload

---
+++ EBudgeting/application/views/masterpos2/pos.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Master POS </title>
+</head>
+<body>
+    <a href="<?php echo site_url('C_masterpos_subpos2/add_pos');?>">Tambah POS</a>
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Nama POS</th>
+            <th>Aksi</th>
+        </tr>
+        <?php 
+        $id = 0;
+        foreach ($sub_pos2 as $nama) :
+            $id++;
+            
+        
+         ?>
+        <tr>
+            <td><?php echo $id; ?></td>
+            <td><?php echo $nama['nama_subpos2'] ?></td>
+            <td>
+            
+                <a href="<?php echo site_url('C_masterpos_subpos2/update_pos/'). $nama['id_subpos2']; ?>">Edit</a>
+                <a href="<?php echo site_url('C_masterpos_subpos2/delete_pos/'). $nama['id_subpos2']; ?>">Hapus</a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+    
+</body>
+</html>
